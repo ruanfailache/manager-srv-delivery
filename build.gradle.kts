3fb15cb6a8986cc8462940d73e3c6b0fef55818a
Ruan Failache <ruanfailache@gmail.com>
2025-02-18 20:57:08 -0300

    fix: data configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,32 +13,33 @@ version = "0.0.1"
 group = "com.manager.api"
 
 val kotlinVersion = project.properties["kotlinVersion"]
+val hibernateVersion = "5.5.7.Final"
+val h2DatabaseVersion = "2.1.214"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    ksp("io.micronaut.data:micronaut-data-processor")
     ksp("io.micronaut:micronaut-http-validation")
     ksp("io.micronaut.serde:micronaut-serde-processor")
 
     implementation("io.micronaut.data:micronaut-data-hibernate-jpa")
-    implementation("io.micronaut.data:micronaut-data-jdbc")
     implementation("io.micronaut.openapi:micronaut-openapi")
     implementation("io.micronaut.openapi:micronaut-openapi-annotations")
     implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
     implementation("io.micronaut.serde:micronaut-serde-jackson")
     implementation("io.micronaut.validation:micronaut-validation")
+    implementation("org.hibernate:hibernate-core:$hibernateVersion")
     implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
-
-    annotationProcessor("io.micronaut.validation:micronaut-validation-processor")
 
     compileOnly("io.micronaut:micronaut-http-client")
 
     runtimeOnly("ch.qos.logback:logback-classic")
     runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
-    runtimeOnly("com.h2database:h2:2.1.214")
+    runtimeOnly("com.h2database:h2:$h2DatabaseVersion")
 
     testImplementation("io.micronaut:micronaut-http-client")
 }
